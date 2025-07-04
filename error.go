6a8c71cb8db8dcaa8a5ae03f1d76e36440fbe72b
Christer Edvartsen <christer.edvartsen@nav.no>
2025-07-04 09:44:08 +0200

    refactor: rename custom error
    BREAKING CHANGE: The Err type has been renamed to Error.

--- error.go
+++ error.go
@@ -6,18 +6,19 @@ import (
 	"github.com/pterm/pterm"
 )
 
-// Err represents an error type that can be used to format error messages for end-users.
-type Err struct {
+// Error represents an error type that can be used to format error messages for end-users.
+type Error struct {
+	// Message is the error message that will be displayed to the end-user.
 	Message string
 }
 
 // Error returns the error message formatted with pterm.Error. This method satisfies the error interface.
-func (e Err) Error() string {
-	return pterm.Error.Sprintf("%s", e.Message)
+func (e Error) Error() string {
+	return pterm.Error.Sprint(e.Message)
 }
 
 // Errorf formats an error message using the provided format and arguments. The returned type satisfies the error
 // interface.
-func Errorf(format string, a ...any) Err {
-	return Err{Message: fmt.Sprintf(format, a...)}
+func Errorf(format string, a ...any) Error {
+	return Error{Message: fmt.Sprintf(format, a...)}
 }

17928a5ff0bd998704036f58c4a5c40bc13f8e15
Christer Edvartsen <christer.edvartsen@nav.no>
2025-09-30 09:22:29 +0200

    build: add options to all bash files
    Also moved some inline script from the workflow to mise tasks.

--- mise/tasks/build.sh
+++ mise/tasks/build.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 #MISE description="Build go code"
+set -euo pipefail
+
 go build ./...
--- mise/tasks/check/golangci-lint.sh
+++ mise/tasks/check/golangci-lint.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 #MISE description="Run golangci-lint"
+set -euo pipefail
+
 go tool github.com/golangci/golangci-lint/cmd/golangci-lint run

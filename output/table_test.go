ca2c3aa0579bbc5ab58f135fd1273d7a739e2097
Christer Edvartsen <christer.edvartsen@nav.no>
2026-04-23 09:41:12 +0200

    test: add tests for table margins

--- output/table_test.go
+++ output/table_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/nais/naistrix/output"
+	"github.com/pterm/pterm"
 )
 
 func TestTable_InvalidData(t *testing.T) {
@@ -77,4 +78,67 @@ func TestTable_ValidData(t *testing.T) {
 			t.Fatalf("unexpected error: %v", err)
 		}
 	})
+}
+
+func TestTable_Margins(t *testing.T) {
+	pterm.DisableStyling()
+	defer pterm.EnableStyling()
+
+	data := []struct {
+		Name string `heading:"Col"`
+	}{
+		{Name: "Val"},
+	}
+
+	tests := []struct {
+		name          string
+		opt           []output.TableOptionFunc
+		expectedStart string
+		expectedEnd   string
+	}{
+		{
+			name:          "no options, default behaviour",
+			opt:           nil,
+			expectedStart: "Col\n",
+			expectedEnd:   "Val\n",
+		},
+		{
+			name:          "top margin",
+			opt:           []output.TableOptionFunc{output.TableWithTopMargin()},
+			expectedStart: "\nCol",
+			expectedEnd:   "Val\n",
+		},
+		{
+			name:          "bottom margin",
+			opt:           []output.TableOptionFunc{output.TableWithBottomMargin()},
+			expectedStart: "Col\n",
+			expectedEnd:   "al\n\n",
+		},
+		{
+			name:          "both margins",
+			opt:           []output.TableOptionFunc{output.TableWithMargins()},
+			expectedStart: "\nCol",
+			expectedEnd:   "al\n\n",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var buf bytes.Buffer
+			table := output.NewTable(&buf, tt.opt...)
+
+			if err := table.Render(data); err != nil {
+				t.Fatalf("unexpected error: %v", err)
+			}
+
+			tbl := buf.String()
+			if got := tbl[:4]; got != tt.expectedStart {
+				t.Fatalf("expected first character in table to be %q, got: %q", tt.expectedStart, got)
+			}
+
+			if got := tbl[len(tbl)-4:]; got != tt.expectedEnd {
+				t.Fatalf("expected last character in table to be %q, got: %q", tt.expectedEnd, got)
+			}
+		})
+	}
 }
